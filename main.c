b415e0aaa51e11da521b51289b9c482919728417
Matheus Costa <matheus.bionicle@gmail.com>
2018-06-15 11:35:33 -0300

    Add readBinary function and number of hits

--- main.c
+++ main.c
@@ -3,49 +3,55 @@
 #define MAX_ADDRESSES 10000
 
 void getPage(int address);
-void binaryToText(char *inputFile, char *outputFile);
+void readBinary(char *inputFile, char const *argv);
+
+FILE *backing_store;
+FILE *address_file;
+
+int pageNumberTBL[16];
+int frameNumberTBL[16];
+int hits = 0;
 
 int main(int argc, char const *argv[]) {
+
     if(argc != 2) {
-        printf("Error: should be ./proj02.out addresses.txt\n");
-        exit(1);
+        perror("Error: ");
+        return(-1);
     }
-    FILE *address_file = fopen(argv[1], "r");
+    readBinary("BACKING_STORE.bin", argv[1]);
     if(address_file == NULL) {
-        printf("Error: couldn't open the file addresses.txt\n");
+        perror("Error: ");
+        return(-1);
     }
+
+    fclose(address_file);
+    fclose(backing_store);
+
+    return 0;
+}
+
+void readBinary(char *inputFile, char const *argv) {
+    unsigned char str[256];
+
+    backing_store = fopen(inputFile, "rb");
+    address_file = fopen(argv, "r");
+
     for(int i = 0; i < MAX_ADDRESSES; ++i) {
         int address;
         fscanf(address_file, "%d", &address);
         getPage(address);
     }
-    return 0;
-}
-
-void binaryToText(char *inputFile, char *outputFile) {
-    unsigned char str[256];
-    unsigned int num;  // assuming 32 bit ints
-    int i, len;
-
-    FILE *finp = fopen(inputFile, "rb");
-    FILE *fout = fopen(outputFile, "w");
-
-    while ((len = fgetc(finp)) != EOF) {
-        fread(str, len, 1, finp);
-        str[len] = '\0';
-        num  = (unsigned int)fgetc(finp) << 24;
-        num |= fgetc(finp) << 16;
-        num |= fgetc(finp) << 8;
-        num |= fgetc(finp);
-        fprintf(fout, "%s %d\n", (char*)str, num);
-    }
-    fclose(finp);
-    fclose(fout);
 }
 
 void getPage(int address) {
-    int sprain, pageNumber;
+    int sprain, pageNumber, frameNumber = -1;
     pageNumber = address >> 8;
     sprain = address & 0xFFFF >> 8;
-    printf("Logical Address: %d\nPage Number: %d\nSprain: %d\n", address, pageNumber, sprain);
+    for(int i = 0; i < 16; ++i){
+        if(pageNumberTBL[i] == pageNumber){
+            frameNumber = frameNumberTBL[i];
+                hits++;
+        }
+    }
+    printf("Logical Address: %d\nPage Number: %d\nSprain: %d\n Hits: %d\n", address, pageNumber, sprain, hits);
 }
